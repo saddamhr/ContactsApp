dff3d4b9724a5eaed9370bc4e73a8e8187bd3582
saddamhr <saddambubt65@gmail.com>
2020-09-28 09:08:07 +0600

    created contact model

---
+++ Contacts.js
@@ -0,0 +1,38 @@
+class Contacts {
+    constructor() {
+        this.contacts = []
+    }
+
+    getAllContacts() {
+        return this.contacts
+    }
+
+    getContactsById(id) {
+        return this.contacts.find(contact => contact.id === id)
+    }
+
+    createContact(contact) {
+        contact.id = this.contacts.length + 1
+        this.contacts.push(contact)
+        return contact
+    }
+
+    updateContactById(id, updateContact) {
+        let index = this.contacts.findIndex(contact => contact.id === id)
+
+        this.contacts[index].name = updateContact.name || this.contacts[index].name
+        this.contacts[index].phone = updateContact.phone || this.contacts[index].phone
+        this.contacts[index].email = updateContact.email || this.contacts[index].email
+
+        return this.contacts[index]
+    }
+
+    deleteContactById(id) {
+        let index = this.contacts.filter(contact => contact.id === id)
+        let deletedObj = this.contacts[index]
+        this.contacts = this.contacts.filter(contact => contact.id !== id)
+        return deletedObj
+    }
+}
+
+exports.contacts = new Contacts()
